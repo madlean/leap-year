fb328bb686ef50ba54182316a02fdc937b8f4103
madlean <71944279+madlean@users.noreply.github.com>
2020-10-11 21:47:43 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int i=4;
+    cout << "Enter the year:" << endl;
+    cin>>x;
+    bool leap_year=true;
+    if(x%4==0){
+        leap_year=true;
+        cout<<x<<"is a leap year"<<endl;
+    }else{
+    cout<<x<<"is not a leap year"<<endl;
+    }
+    return 0;
+}
+//GROUP MEMBERS
+Madlean Auma Matsotso-118468
+Cecilia Edna Teng'ó-117666
+Patricia Ng'ang'a-120085
+Christine Imelda Akinyi-135179
